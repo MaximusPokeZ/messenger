30f58cabb8b959febf4b08c7dc6cff2843f52fab
ykwais <fedorkovaleks04@gmail.com>
2025-05-22 14:21:45 +0300

    javafx added

---
+++ frontend/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.frontend to javafx.fxml;
+    exports org.example.frontend;
+}
