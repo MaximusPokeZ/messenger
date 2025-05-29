8817e1f1b165bac0caedb65610eef3b7e855e6f9
MaximusPokeZ <mzaslavtsev@gmail.com>
2025-05-30 00:24:26 +0300

    new branch for ykwais

---
+++ backend/src/main/java/org/example/backend/services/impl/ChatServiceImpl.java
@@ -0,0 +1,75 @@
+package org.example.backend.services.impl;
+
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.ServerCallStreamObserver;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.example.backend.dto.responses.UsernameResponse;
+import org.example.shared.ChatProto;
+import org.example.shared.ChatServiceGrpc;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@GrpcService
+public class ChatServiceImpl extends ChatServiceGrpc.ChatServiceImplBase {
+
+    private final Map<String, StreamObserver<ChatProto.ChatMessage>> clients = new ConcurrentHashMap<>();
+
+    @Override
+    public void connect(ChatProto.ConnectRequest request, StreamObserver<ChatProto.ChatMessage> responseObserver) {
+        String userId = request.getUserName();
+        clients.put(userId, responseObserver);
+
+
+        if (responseObserver instanceof ServerCallStreamObserver) {
+            ServerCallStreamObserver<ChatProto.ChatMessage> serverObserver =
+                    (ServerCallStreamObserver<ChatProto.ChatMessage>) responseObserver;
+
+            serverObserver.setOnCancelHandler(() -> {
+                clients.remove(userId);
+                System.out.println("User " + userId + " disconnected");
+            });
+        }
+    }
+
+    @Override
+    public void sendMessage(ChatProto.SendMessageRequest request, StreamObserver<ChatProto.SendMessageResponse> responseObserver) {
+        String recipientId = request.getToUserName();
+        StreamObserver<ChatProto.ChatMessage> recipientStream = clients.get(recipientId);
+        boolean delivered = false;
+
+        if (recipientStream != null) {
+            try {
+                ChatProto.ChatMessage message = ChatProto.ChatMessage.newBuilder()
+                        .setFromUserName(request.getFromUserName())
+                        .setText(request.getText())
+                        .setDateTime(request.getDateTime())
+                        .build();
+
+
+                recipientStream.onNext(message);
+                delivered = true;
+            } catch (StatusRuntimeException e) {
+
+                clients.remove(recipientId);
+            }
+        }
+
+        // Возвращаем результат отправителю
+        ChatProto.SendMessageResponse response = ChatProto.SendMessageResponse.newBuilder()
+                .setDelivered(delivered)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    public List<UsernameResponse> getOnlineUsernames() {
+        return clients.keySet().stream().map(UsernameResponse::new).toList();
+    }
+}
